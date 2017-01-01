2892b929ef73c9f92ca549d16342423b37cadf2d
Bihan Zhuang <bihan.zhuang@duke.edu>
2017-01-01 17:34:38 -0600

    added files

---
+++ js/myjs.js
@@ -0,0 +1,50 @@
+/*
+  Code by Gabriel Nunes
+*/
+
+function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+
+var currentQuote = '', currentAuthor = '';
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(response) {
+      var r = JSON.parse(response);
+      currentQuote = r.quote;
+      currentAuthor = r.author;
+      
+      $(".quote-text").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#text').text(r.quote);
+        });
+
+      $(".quote-author").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#author').html(r.author);
+        });
+    }
+  });
+}
+
+$(document).ready(function() {
+  getQuote();
+  $("#new-quote").on("click", getQuote);
+});
